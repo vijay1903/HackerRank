1a5c942af7b68361cf01a7e177f36aea86d72499
Vijay Vishwakarma <vijayv@mail.usf.edu>
2018-03-15 21:17:15 -0400

    Create Day 4: Count Objects.js

---
+++ 10 Days of JavaScript/Day 4: Count Objects.js
@@ -0,0 +1,46 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+function getCount(objects) {
+    var count = 0, i = 0;
+    //console.log(objects[0].x);
+    while(i != objects.length){
+        if(objects[i].x == objects[i].y){
+            count++;
+        }
+        i++;
+    }
+    return count;
+}
+function main() {
+    const n = +(readLine());
+    let objects = [];
+    
+    for (let i = 0; i < n; i++) {
+        const [a, b] = readLine().split(' ');
+        
+        objects.push({x: +(a), y: +(b)});
+    }
+    
+    console.log(getCount(objects));
+}
