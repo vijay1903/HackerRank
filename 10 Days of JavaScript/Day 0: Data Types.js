3938fc21a4330b03af8607140882ece1c7fe575d
Vijay Vishwakarma <vijayv@mail.usf.edu>
2018-03-15 21:08:54 -0400

    Create Day 0: Data Types.js

---
+++ 10 Days of JavaScript/Day 0: Data Types.js
@@ -0,0 +1,10 @@
+
+function performOperation(secondInteger, secondDecimal, secondString) {
+    const firstInteger = 4;
+    const firstDecimal = 4.0;
+    const firstString = 'HackerRank ';
+    console.log(parseInt(firstInteger)+parseInt(secondInteger));
+    console.log(parseFloat(secondDecimal)+firstDecimal);
+    console.log(firstString+secondString);
+    
+}
