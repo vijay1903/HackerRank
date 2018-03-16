a23e4615b6f960474291b821cb235d6981e81874
Vijay Vishwakarma <vijayv@mail.usf.edu>
2018-03-15 21:20:30 -0400

    Create Day 6: JavaScript Dates.js

---
+++ 10 Days of JavaScript/Day 6: JavaScript Dates.js
@@ -0,0 +1,41 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+function getDayName(dateString) {
+    let dayName;
+    var day_list = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    var d = new Date(dateString);
+    dayName = day_list[d.getDay()];
+    
+    return dayName;
+}
+
+function main() {
+    const d = +(readLine());
+    
+    for (let i = 0; i < d; i++) {
+        const date = readLine();
+        
+        console.log(getDayName(date));
+    }
+}
