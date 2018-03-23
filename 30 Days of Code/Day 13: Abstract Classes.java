09f189e1096d11be9a3c2a991c26f0cea7a4704c
Vijay Vishwakarma <vijayv@mail.usf.edu>
2018-03-23 00:18:47 -0400

    Create Day 13: Abstract Classes.java

---
+++ 30 Days of Code/Day 13: Abstract Classes.java
@@ -0,0 +1,13 @@
+class MyBook extends Book{
+    int p;
+    MyBook(String title, String author,int price){
+        super(title,author);
+        this.p = price;
+    }
+
+    void display(){
+        System.out.println("Title: "+title);
+        System.out.println("Author: "+author);
+        System.out.println("Price: "+p);
+    }
+}
